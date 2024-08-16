1edd48705126affa1e0c71e24772f5c45fcb4144
putrisp <prikustyowati.putri@gmail.com>
2024-08-16 09:32:24 +0700

    variables, promises & nested closures

--- cypress/integration/automation-test-store/variables-and-cy-commands.js
+++ cypress/integration/automation-test-store/variables-and-cy-commands.js
@@ -18,7 +18,7 @@ describe("Verifying variables, cypress commands and jquery commands", () => {
     cy.get("a[href*='product/category&path=").contains("Makeup").click();
     cy.get("a[href*='product/category&path=").contains("Skincare").click();
   });
-  it.only("Navigating to specific product page", () => {
+  it("Navigating to specific product page", () => {
     //cypress code
     cy.visit("https://automationteststore.com/");
     cy.get("a[href*='product/category&path=").contains("Makeup").click();
@@ -28,5 +28,23 @@ describe("Verifying variables, cypress commands and jquery commands", () => {
       cy.log("Found header text: " + headertext);
       expect(headertext).to.eq('Makeup')
     });
+  });
+  it.only("Validate properties of the Contact Us Page", () => {
+    cy.visit('https://automationteststore.com/index.php?rt=content/contact')
+
+    //uses cypress commands and chaining
+    cy.contains('#ContactUsFrm','Contact Us Form').find('#field_11').should('contain','First name:')
+
+    //jquery approach
+    cy.contains('#ContactUsFrm','Contact Us Form').then(text => {
+      const fistNameText = text.find('#field_11').text()
+      expect(fistNameText).to.contain('First name')
+    })
+
+    //embedded commands (closure)
+    cy.get('#field_11').then(fnText => {
+      cy.log(fnText.text())
+      cy.log(fnText)
+    })
   });
 });
