c8ac4e7950764b91126ac646582f0ac528869f64
putrisp <prikustyowati.putri@gmail.com>
2024-08-23 17:02:32 +0700

    handling multiple browser tab

--- cypress/integration/webdriver-uni/contact-us.js
+++ cypress/integration/webdriver-uni/contact-us.js
@@ -3,11 +3,12 @@
 describe('Test Contact Us from via WebdriverUni', () => {
     it('Should be able to submit a successful submission via contact us form', () => {
         //cypress code
-       cy.visit('https://www.webdriveruniversity.com/Contact-Us/contactus.html')
+       cy.visit('https://www.webdriveruniversity.com')
+       //multiple browser tab
+       cy.get('#contact-us').invoke('removeAttr','target').click({force:true})
        cy.document().should('have.property','charset').and('eq','UTF-8')
        cy.title().should('include','WebDriver | Contact Us')
        cy.url().should('include','contactus')
-       //cy.get('#contact-us').click({force:true})
        cy.get('[name="first_name"]').type("Poetri")
        cy.get('[name="last_name"]').type("Sri")
        cy.get('[name="email"]').type("haha@gmail.co")
@@ -19,8 +20,9 @@ describe('Test Contact Us from via WebdriverUni', () => {
     });
     it('Should not be able to submit a successful submission via contact us form as all field are required ', () => {
         //cypress code
-        cy.visit('https://www.webdriveruniversity.com/Contact-Us/contactus.html')
-       //cy.get('#contact-us').click({force:true})
+        cy.visit('https://www.webdriveruniversity.com')
+       //multiple browser tab
+       cy.get('#contact-us').invoke('removeAttr','target').click({force:true})
        cy.get('[name="first_name"]').type("Poetri")
        cy.get('[name="last_name"]').type("Sri")
        cy.get('textarea.feedback-input').type("learn cypress")
